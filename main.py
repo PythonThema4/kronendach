9bdd2fadfe7928b28f611c93ecaac0e26a7f2e46
alexhoefner <48288865+alexhoefner@users.noreply.github.com>
2020-01-15 12:10:19 +0100

    erste versuche

--- main.py
+++ main.py
@@ -1,4 +1,50 @@
+import numpy as np
 
-for i in range(0,11):
-    x = i
+
+fobj = open('Daten/Waldpunktwolke_short.txt', "r")
+print fobj
+
+pointlist = []
+
+
+#read in
+next(fobj)
+for line in fobj:
+    line = line.strip()
+    line= line.split("\t")   
+    pointlist.append(line)
+fobj.close
+
+pointarray = np.array(pointlist) 
+pointarray = pointarray.astype(float)  
+
+#print pointlist
+print "--------"
+
+#max values
+maxvals = np.amax(pointarray,axis=0) 
+xmax=maxvals[0]
+ymax=maxvals[1]
+print "xmax:",xmax
+print "ymax:",ymax
+#min values
+minvals = np.amin(pointarray,axis=0) 
+xmin=minvals[0]
+ymin=minvals[1]
+print "xmin:",xmin
+print "ymin:",ymin
+#ratio
+nrows= np.ceil(ymax-ymin)
+ncols= np.ceil(xmax-xmin)
+
+print nrows
+print ncols
+#create Extent Raster
+array = np.empty(nrows,ncols)
+
+for x in np.arange(xmin,xmin+ncols):
     print x
+    for y in np.arange(ymin,ymin+nrows):
+        print y
+        
+

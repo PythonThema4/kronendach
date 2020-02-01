25a86e13dd655dfb3d34630c1aba13eb9165664a
Andreas Summer <andreas.summer@stundent.uibk.ac.at>
2020-02-01 16:42:06 +0100

    npnan probleme behoben_output raster probleme

--- Untitled-1.py
+++ Untitled-1.py
@@ -42,8 +42,7 @@ import statistics
 #-------------------------------
 #File einlesen:
 #-------------------------------
-fobj = open('Daten/Waldpunktwolke_5000.txt', "r") #Input File
-InputSize = "5000"
+fobj = open('Daten/Waldpunktwolke_1000.txt', "r") #Input File
 
 pointlist = []
 
@@ -108,95 +107,187 @@ nrows= int(np.ceil(ymax-ymin))
 ncols= int(np.ceil(xmax-xmin))
 z=2
 
-# Erstellen der verschiedenen Arrays
-
 bodencountout_array = np.empty((nrows,ncols))
 bodencountout_array.fill(0)
-
-bodenhoehencountout_array = np.empty((nrows,ncols))
-bodenhoehencountout_array[:] = np.NaN
 
 bodenhoehen_array = np.empty((nrows,ncols))
 bodenhoehen_array[:] = np.NaN
 
+
 vegcount_array = np.empty((nrows,ncols))
 vegcount_array.fill(0)
 
-vegcount_higher2_array = np.empty((nrows,ncols))
-vegcount_higher2_array.fill(0)
+# veghoeheout_array = np.empty((nrows,ncols))
+# veghoeheout_array.fill(0)
 
-veghigher2_hoehen_array  = np.empty((nrows,ncols))
-veghigher2_hoehen_array[:] = np.NaN
+# vegstdout_array = np.empty((nrows,ncols))
+# vegstdout_array.fill(0)
+
+# vegslicecountout_array = np.empty((nrows,ncols))
+# vegslicecountout_array.fill(0)
+
+# vegsliceverh_array = np.empty((nrows,ncols))
+# vegsliceverh_array.fill(0)
+
+# veghoehe_ab2_array = np.empty((nrows,ncols))
+# veghoehe_ab2_array.fill(0)
 
 vegpoints_array = np.empty((nrows,ncols))
 vegpoints_array[:] = np.NaN
 
 bodenpoints_array = np.empty((nrows,ncols))
 bodenpoints_array[:] = np.NaN
+#bodenpoints_array.fill(0)
 
-veghigher2_count_array = np.empty((nrows,ncols))
-veghigher2_count_array[:] = np.NaN
 
 # %%
 #----------------------------------
 #Bodenpunkte zaehlen:
 #----------------------------------
-#Cellsize = float(raw_input("Bitte geben Sie die gewuenschte Rasterzellengroesse an:")) #Raw Input funktioniert in VisualStudio nicht
-Cellsize = 1
+#bodencount = 0
+#countlist =[]
+#arraycount_x = 0
+#arraycount_y = 0
+#bodenhoehenlist = []
 
-print("Bodenpunkte in Rasterzellen aufteilen und Mittelwert pro Zelle berechnen:")
-printProgressBar(0, len(bodenarray), prefix = 'Progress:', suffix = 'Complete', length = 50)
-j=0
+Cellsize=1
+
 for i in bodenarray:
-    printProgressBar(j+1, len(bodenarray), prefix = 'Progress:', suffix = 'Complete', length = 50)
-    j+=1
+ #   print (i)
     x=i[0]
     y=i[1]
     height = i[2]
     gx = int((x -xmin)/Cellsize)
     gy = int((y -ymax)/-Cellsize)  
     bodencountout_array[gy][gx] +=1
-    bodenhoehencountout_array[gy][gx] +=1
-    if np.isnan(bodenhoehencountout_array[gy][gx]) == True:
+    #bodenhoehenlist.append(i[2])
+    if np.isnan(bodenhoehen_array[gy][gx]) == True:
         bodenhoehen_array[gy][gx] = i[2]
     else:
         bodenhoehen_array[gy][gx] = np.mean([bodenhoehen_array[gy][gx],i[2]])
+
+
+#for i in bodenpoints_array:
+ #   print (i)
+
+#     bodenhoehenout_array[gy,gx] = np.mean(bodenpoints_array,axis=0)
+
+# print (bodenhoehenout_array)   
+
+#bodenhoehenout_array= [nrows,ncols]=np.mean(bodenpoints_array[)
+
+#print (bodenhoehenout_array)
+    # for i in bodenarray[-1]:
+    # print (i)
+    # xnew=i[0]
+    # y2=i[1]
+    # gx2 = int((x2 -xmin)/Cellsize)
+    # gy2 = int((y2 -ymax)/-Cellsize)
+    # if x == gx2 and y == gy2:
+    #     bodenhoehenlist.append(i[2])
+    # else:
+    #     bodenhoehenout_array[gy,gx]=np.mean(bodenhoehenlist)
+    #     bodenhoehenlist=[]
+    #bodenpoints_array[gy,gx]=i
+   # bodenpoints_array[gy,gx] = i
+    #  bodencountout_array[gy,gx]=1
+    # if x <= i[0] < gx+1 and gy >= i[1] > gy-1:
+    #    bodenhoehenlist.append(i[2])
+    #bodenhoehenout_array[gy,gx]=np.mean(bodenhoehenlist)
+    #bodenhoehenlist=[]
+    #print(bodencountout_array)
+    #bodenpoints_array[gy,gx]=i
+
+
+# print("Bodenpunkte zuordnen:")
+# printProgressBar(0, len(np.arange(ymax,ymax-nrows,-1)), prefix = 'Progress:', suffix = 'Complete', length = 50)
+# j=0
+# for y in np.arange(ymax,ymax-nrows,-1):
+#     printProgressBar(j+1, len(np.arange(ymax,ymax-nrows,-1)), prefix = 'Progress:', suffix = 'Complete', length = 50)
+#     j+=1
+#     # print("yRichtung:",y)
+#     # print("ArraypositionY:",arraycount_y)
+#     for x in np.arange(xmin,xmin+ncols,1):
+#         # print("xRichtung:",x)
+#         # print("ArraypositionX:",arraycount_x)
+#         for i in bodenarray:
+#             if x <= i[0] < x+1 and y >= i[1] > y-1:
+#                 bodencount+=1
+#                 bodenhoehenlist.append(i[2])
+#         bodencountout_array[arraycount_y][arraycount_x]=bodencount
+#         bodenhoehenout_array[arraycount_y][arraycount_x]=np.mean(bodenhoehenlist)
+#         arraycount_x+=1     
+#         bodencount=0
+#         bodenhoehenlist=[]
+#     arraycount_x=0
+#     arraycount_y+=1
+    # print("xRichtungEnde")
+    #bodenhoehenout_array[np.isnan(bodenhoehenout_array)] = 0
+
+
 
 
 # %%
 #----------------------------------
 #Mittlere Hoehe der VegPunkte:
 #----------------------------------
-print("Vegetationspunkte in Rasterzellen aufteilen und erstellen eines Array mit Punkten uber 2 Meter:")
-printProgressBar(0, len(vegarray), prefix = 'Progress:', suffix = 'Complete', length = 50)
-j=0
+#vegcount = 0
+#vegslicecount = 0
+
+# countlist =[]
+# arraycount_x = 0
+# arraycount_y = 0
+# veghoehenlist = []
+
+# print("VegPunkte zuordnen:")
+# printProgressBar(0, len(np.arange(ymax,ymax-nrows,-1)), prefix = 'Progress:', suffix = 'Complete', length = 50)
+# j=0
+
+Cellsize=1
+
 for i in vegarray:
-    printProgressBar(j+1, len(vegarray), prefix = 'Progress:', suffix = 'Complete', length = 50)
-    j+=1
     x=i[0]
     y=i[1]
     height = i[2]
     #print(height-bodenhoehen_array[gy][gx])
     gx = int((x -xmin)/Cellsize)
     gy = int((y -ymax)/-Cellsize)
-    vegcount_array[gy,gx]+=1
+    
     if height-bodenhoehen_array[gy][gx] >= 2:
-        vegcount_higher2_array[gy,gx]+=1
-        veghigher2_count_array[gy][gx]+=1
-        if np.isnan(veghigher2_count_array[gy][gx]) == True:
-            veghigher2_hoehen_array[gy][gx] = (i[2]-bodenhoehen_array[gy][gx])
-        else:
-            veghigher2_hoehen_array[gy][gx] = np.mean([veghigher2_hoehen_array[gy][gx],(i[2]-bodenhoehen_array[gy][gx])])
+        vegcount_array[gy,gx]+=1
+        #print(">2:",gy,gx)
+         
 
 
-#Alternativ Code 
-#         veghigher2_hoehen_array[gy][gx] = (i[2]-bodenhoehen_array[gy][gx])
-# veghigher2_hoehen_array[gy][gx] = np.mean([veghigher2_hoehen_array[gy][gx],i[2]])
-# # # Zwei obere Zeilen = Vereinfachung der unteren Schleife - bei Boden auch noch zu machen            
-  
+# for y in np.arange(ymax,ymax-nrows,-1):
+#     printProgressBar(j+1, len(np.arange(ymax,ymax-nrows,-1)), prefix = 'Progress:', suffix = 'Complete', length = 50)
+#     j+=1
+#     # print("yRichtung:",y)
+#     # print("ArraypositionY:",arraycount_y)
+#     for x in np.arange(xmin,xmin+ncols,1):
+#         # print("xRichtung:",x)
+#         # print("ArraypositionX:",arraycount_x)
+#         for i in vegarray:
+#             if x <= i[0] < x+1 and y >= i[1] > y-1: #Hier sind wir in der jeweiligen Zelle
+#                 veghoehenlist.append(i[2])
+#                 vegcount+=1
+#                 if i[2] >= ((max(veghoehenlist))-1):
+#                     vegslicecount+=1
+        
+    #     #vegpoints_array[arraycount_y][arraycount_x]=(veghoehenlist)
+    #     veghoeheout_array[arraycount_y][arraycount_x]=np.mean(veghoehenlist) #Mittlere Hoehe der VegPunkte
+    #     vegstdout_array[arraycount_y][arraycount_x]=np.std(veghoehenlist) #Standardabweichung der VegPunkte
+    #     #vegcountout_array[arraycount_y][arraycount_x]=vegcount #Anzahl der Vegetationspunkte in Zelle
+    #     vegslicecountout_array[arraycount_y][arraycount_x]=vegslicecount #Anzahl der Vegetationspunkte in Zelle
 
-
-
+    #     #forif veghoehenlist ==
+    #      #   print (max(veghoehenlist))
+    #     arraycount_x+=1     
+    #     vegcount=0
+    #     vegslicecount=0
+    #     veghoehenlist=[]
+    # arraycount_x=0
+    # arraycount_y+=1
     # #print("xRichtungEnde") 
     # #vegstdout_array[np.isnan(vegstdout_array)] = 0
     # #veghoeheout_array[np.isnan(veghoeheout_array)] = 0
@@ -204,18 +295,37 @@ for i in vegarray:
 
 # %%
 #-------------------------------
+#Tatsaechliche VegHoehe: nDom
+#-------------------------------
+#vegtathoehe_array = np.empty((nrows,ncols))
+#vegtathoehe_array = veghoeheout_array - bodenhoehenout_array
+
+
+# %%
+#-------------------------------
 #Index anzahl veg zu bodenpunkte veg/(veg-boden):
 #-------------------------------
 indexarray1 = np.empty((nrows,ncols))
-indexarray1 = (vegcount_array/(vegcount_array+bodencountout_array))
+indexarray1 = vegcount_array/(vegcount_array+bodencountout_array)
 
 
 # %%
 #-------------------------------
 #Index anzahl veg ueber 2 m zu bodenpunkte veg/(veg-boden):
 #-------------------------------
-indexhoehen2m = np.empty((nrows,ncols))
-indexhoehen2m = (vegcount_higher2_array/(vegcount_higher2_array+bodencountout_array))
+#veghoehe_ab2_array = vegtathoehe_array >2
+#print (veghoehe_ab2_array)
+#indexhoehen2m = np.empty((nrows,ncols))
+#indexhoehen2m = vegcountout_array/(vegcountout_array+bodencountout_array)
+
+
+# %%
+# for y in np.arange(ymax,ymax-nrows,-1):
+#     # print("yRichtung:",y)
+#     # print("ArraypositionY:",arraycount_y)
+#     for x in np.arange(xmin,xmin+ncols,1):
+#         if vegtathoehe_array[x][y]>=
+#         vegsliceverh_array = vegslicecountout_array/vegcountout_array #verhaeltnis von veg im obersten meter zu gesamte vegPunkte
 
 
 # %%
@@ -226,7 +336,7 @@ indexhoehen2m = (vegcount_higher2_array/(vegcount_higher2_array+bodencountout_ar
 
 #Boden Raster
 driver = gdal.GetDriverByName("GTiff")
-dataset = driver.Create("Export/BodenCount_%s.tif"  % (InputSize), ncols, nrows, 1, gdal.GDT_Float32)
+dataset = driver.Create("Export/BodenCount_1000.tif", ncols, nrows, 1, gdal.GDT_Float32)
 dataset.SetGeoTransform((xmin,1,0,ymax,0,-1))
 
 #Koordinatensystem definieren:
@@ -244,7 +354,7 @@ dataset.FlushCache()
 # %%
 # #Bodenhoehen Raster
 driver = gdal.GetDriverByName("GTiff")
-dataset = driver.Create("Export/BodenHoehen_%s.tif" % (InputSize), ncols, nrows, 1, gdal.GDT_Float32)
+dataset = driver.Create("Export/BodenHoehen_1000.tif", ncols, nrows, 1, gdal.GDT_Float32)
 dataset.SetGeoTransform((xmin,1,0,ymax,0,-1))
 
 # #Koordinatensystem definieren:
@@ -263,7 +373,7 @@ dataset.FlushCache()
 
 #Vegetation Zaehler Raster
 driver = gdal.GetDriverByName("GTiff")
-dataset = driver.Create("Export/VegCount_%s.tif" % (InputSize), ncols, nrows, 1, gdal.GDT_Float32)
+dataset = driver.Create("Export/VegCount_1000.tif", ncols, nrows, 1, gdal.GDT_Float32)
 dataset.SetGeoTransform((xmin,1,0,ymax,0,-1))
 
 #Koordinatensystem definieren:
@@ -281,20 +391,20 @@ dataset.FlushCache()
 # %%
 
 # #Vegetation Mittlere Hoehe Raster
-driver = gdal.GetDriverByName("GTiff")
-dataset = driver.Create("Export/VegHoehen_%s.tif" % (InputSize), ncols, nrows, 1, gdal.GDT_Float32)
-dataset.SetGeoTransform((xmin,1,0,ymax,0,-1))
+# driver = gdal.GetDriverByName("GTiff")
+# dataset = driver.Create("Export/VegHoehen_1000.tif", ncols, nrows, 1, gdal.GDT_Float32)
+# dataset.SetGeoTransform((xmin,1,0,ymax,0,-1))
 
 # #Koordinatensystem definieren:
-dstSRS = osr.SpatialReference()
-dstSRS.ImportFromEPSG(32632)
-dest_wkt = dstSRS.ExportToWkt()
+# dstSRS = osr.SpatialReference()
+# dstSRS.ImportFromEPSG(32632)
+# dest_wkt = dstSRS.ExportToWkt()
 
-dataset.SetProjection(dest_wkt)
+# dataset.SetProjection(dest_wkt)
 
 # #Raster ausgeben:
-bandout = dataset.GetRasterBand(1).WriteArray(veghigher2_hoehen_array)
-dataset.FlushCache()
+# bandout = dataset.GetRasterBand(1).WriteArray(vegtathoehe_array)
+# dataset.FlushCache()
 
 print ("done1")
 # %%
@@ -315,10 +425,30 @@ print ("done1")
 # bandout = dataset.GetRasterBand(1).WriteArray(vegstdout_array)
 # dataset.FlushCache()
 
+
+# %%
+
+#Vegetation Standardabweichung Hoehe Raster
+# driver = gdal.GetDriverByName("GTiff")
+# dataset = driver.Create("Export/VegStd_1000_float64.tif", ncols, nrows, 1, gdal.GDT_Float64)
+# dataset.SetGeoTransform((xmin,1,0,ymax,0,-1))
+
+# #Koordinatensystem definieren:
+# dstSRS = osr.SpatialReference()
+# dstSRS.ImportFromEPSG(32632)
+# dest_wkt = dstSRS.ExportToWkt()
+
+# dataset.SetProjection(dest_wkt)
+
+# #Raster ausgeben:
+# bandout = dataset.GetRasterBand(1).WriteArray(vegstdout_array)
+# dataset.FlushCache()
+
+
 # %%
 #Index Verhaeltnis VegPunkte zu Bodenpunkte Raster
 driver = gdal.GetDriverByName("GTiff")
-dataset = driver.Create("Export/Index1_%s.tif" % (InputSize), ncols, nrows, 1, gdal.GDT_Float32)
+dataset = driver.Create("Export/Index1_1000.tif", ncols, nrows, 1, gdal.GDT_Float32)
 dataset.SetGeoTransform((xmin,1,0,ymax,0,-1))
 
 #Koordinatensystem definieren:
@@ -332,39 +462,10 @@ dataset.SetProjection(dest_wkt)
 bandout = dataset.GetRasterBand(1).WriteArray(indexarray1)
 dataset.FlushCache()
 
+print ("finalDone")
+
 
 # %%
-#Index Verhaeltnis VegPunkte zu Bodenpunkte Raster
-driver = gdal.GetDriverByName("GTiff")
-dataset = driver.Create("Export/Index2_higher2_%s.tif" % (InputSize), ncols, nrows, 1, gdal.GDT_Float32)
-dataset.SetGeoTransform((xmin,1,0,ymax,0,-1))
 
-#Koordinatensystem definieren:
-dstSRS = osr.SpatialReference()
-dstSRS.ImportFromEPSG(32632)
-dest_wkt = dstSRS.ExportToWkt()
 
-dataset.SetProjection(dest_wkt)
 
-#Raster ausgeben:
-bandout = dataset.GetRasterBand(1).WriteArray(indexhoehen2m)
-dataset.FlushCache()
-
-# # %%
-# #Index Verhaeltnis VegPunkte zu Bodenpunkte Raster Saga
-# driver = gdal.GetDriverByName("AAIGrid")
-# dataset = driver.Create("Export/Index2_higher2_%s.tif" % (InputSize), ncols, nrows, 1, gdal.GDT_Float32)
-# #dataset.SetGeoTransform((xmin,1,0,ymax,0,-1))
-
-# #Koordinatensystem definieren:
-# dstSRS = osr.SpatialReference()
-# dstSRS.ImportFromEPSG(32632)
-# dest_wkt = dstSRS.ExportToWkt()
-
-# dataset.SetProjection(dest_wkt)
-
-# #Raster ausgeben:
-# bandout = dataset.GetRasterBand(1).WriteArray(vegstdout_array)
-# dataset.FlushCache()
-
-print ("finalDone")
